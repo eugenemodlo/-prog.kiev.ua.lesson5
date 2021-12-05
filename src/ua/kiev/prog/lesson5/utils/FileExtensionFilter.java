3f2a6ece0307b423ccda1537416b4380d7dc59d2
eugenemodlo <eugenemodlo@gmail.com>
2021-12-05 19:50:11 +0200

    add filter file by extension class

---
+++ src/ua/kiev/prog/lesson5/utils/FileExtensionFilter.java
@@ -0,0 +1,33 @@
+package ua.kiev.prog.lesson5.utils;
+
+import java.io.File;
+import java.io.FileFilter;
+
+public class FileExtensionFilter implements FileFilter {
+    private final String[] acceptedFileExtensions;
+
+    public FileExtensionFilter(String[] array) {
+        super();
+        this.acceptedFileExtensions = array;
+    }
+
+    private boolean checkExtension(String ext) {
+        for (String acceptedExtension : acceptedFileExtensions) {
+            if (acceptedExtension.equals(ext)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public boolean accept(File pathName) {
+        String DOT = ".";
+        int pointerIndex = pathName.getName().lastIndexOf(DOT);
+        if (pointerIndex == -1) {
+            return false;
+        }
+        String ext = pathName.getName().substring(pointerIndex + 1);
+        return checkExtension(ext);
+    }
+}
