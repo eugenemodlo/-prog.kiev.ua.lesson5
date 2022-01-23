b67428f4b436432fd5fc58ba1688a835c7b33540
eugenemodlo <eugenemodlo@gmail.com>
2022-01-23 18:31:25 +0200

    add copy files methods

--- src/ua/kiev/prog/lesson5/utils/FileUtility.java
+++ src/ua/kiev/prog/lesson5/utils/FileUtility.java
@@ -1,6 +1,8 @@
 package ua.kiev.prog.lesson5.utils;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Objects;
 
@@ -9,24 +11,56 @@ public class FileUtility {
     }
 
     public void CopyFiles(String inputPath, String outputPath, String fileExtension) {
-
-        final String DELIMITER = "/";
-        final String DOT = ".";
         FileExtensionFilter filter = new FileExtensionFilter(fileExtension);
         File inputDir = new File(inputPath);
 
         for (File file : Objects.requireNonNull(inputDir.listFiles(filter))) {
-            String copyFileFullPath = new StringBuilder(outputPath).
-                    append(DELIMITER).
-                    append(file.getName()).
-                    append(DOT).append(fileExtension).toString();
-
-            File newFile = new File(copyFileFullPath);
-
-
-            System.out.println(file.getName());
+            File newFile = new File(outputPath, file.getName());
+            if (copyFile(file, newFile)) {
+                System.out.println("File: " + file.getName() + " was copied successfully.");
+            }
         }
 
+    }
 
+    private boolean copyFile(File inputFile, File outputFile) {
+        if (createNewFile(outputFile)) {
+            try (FileInputStream fis = new FileInputStream(inputFile);
+                 FileOutputStream fos = new FileOutputStream(outputFile)) {
+                byte[] buffer = new byte[1024];
+                int byteRead = 0;
+
+                while ((byteRead = fis.read(buffer)) > 0) {
+                    fos.write(buffer, 0, byteRead);
+                }
+                return true;
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return false;
+    }
+
+    private boolean createNewFile(File file) {
+        File dir = new File(file.getParent());
+        if (!dir.exists()) {
+            if (!dir.mkdir()){
+                System.out.println("Can't create directory at: " + file.getPath());
+                return false;
+            }
+        }
+
+        try {
+            if (file.createNewFile()) {
+                System.out.println("Create");
+                return true;
+            } else {
+                System.out.println("Can't create the file:" + file.getName());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return false;
     }
 }
