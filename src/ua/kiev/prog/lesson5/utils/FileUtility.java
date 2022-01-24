d6df9a8ebe61f8e077f95f230016a59f922315bf
eugenemodlo <eugenemodlo@gmail.com>
2022-01-24 19:35:02 +0200

    issue fixes

--- src/ua/kiev/prog/lesson5/utils/FileUtility.java
+++ src/ua/kiev/prog/lesson5/utils/FileUtility.java
@@ -1,14 +1,30 @@
 package ua.kiev.prog.lesson5.utils;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
 import java.util.Objects;
+import java.util.SortedSet;
 
 public class FileUtility {
     public FileUtility() {
     }
+
+//    public File fileCompareByWord(File stFile, File ndFile, String outFileFullPath) {
+//        File outFile = new File(outFileFullPath);
+//
+//        try (FileReader firstFile = new FileReader(stFile);
+//             FileReader secondFile = new FileReader(ndFile)) {
+//
+//            List<String> firstList = Files.readAllLines(new Path stFile.getPath());
+//
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//
+//        return outFile;
+//    }
 
     public void CopyFiles(String inputPath, String outputPath, String fileExtension) {
         FileExtensionFilter filter = new FileExtensionFilter(fileExtension);
@@ -43,23 +59,27 @@ public class FileUtility {
     }
 
     private boolean createNewFile(File file) {
-        File dir = new File(file.getParent());
-        if (!dir.exists()) {
-            if (!dir.mkdir()){
-                System.out.println("Can't create directory at: " + file.getPath());
-                return false;
+        if (!file.exists()) {
+            File dir = new File(file.getParent());
+            if (!dir.exists()) {
+                if (!dir.mkdir()) {
+                    System.out.println("Can't create directory at: " + file.getPath());
+                    return false;
+                }
             }
-        }
 
-        try {
-            if (file.createNewFile()) {
-                System.out.println("Create");
-                return true;
-            } else {
-                System.out.println("Can't create the file:" + file.getName());
+            try {
+                if (file.createNewFile()) {
+                    System.out.println("Create");
+                    return true;
+                } else {
+                    System.out.println("Can't create the file:" + file.getName());
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-        } catch (IOException e) {
-            e.printStackTrace();
+        } else {
+            System.out.println("File: " + file.getName() + " already exist");
         }
         return false;
     }
